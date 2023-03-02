a879e36bb06cfeda64168c5b67912318be15b1dd
Aniketjiotode <aniketjiotode07@gmail.com>
2023-03-02 13:54:44 +0530

    Added: UC_2 Changes

--- Payroll_Service.sql
+++ Payroll_Service.sql
@@ -2,3 +2,13 @@
 
 CREATE DATABASE payroll_service;
 USE payroll_service;
+
+--UC_2
+
+CREATE TABLE empolyee_payroll
+(
+  id INT IDENTITY PRIMARY KEY NOT NULL,
+  name VARCHAR(150) NOT NULL,
+  salary INT NOT NULL,
+  start DATE NOT NULL,
+);
